32f7541f0ddaaae4815c871a85781ee816d0c500
Martin Leonardo Martinez Hernandez <martin.martinezh@colvatel.com>
2025-09-02 16:41:20 -0500

    kk

--- tienda/settings.py
+++ tienda/settings.py
@@ -123,12 +123,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+
+# Carpeta donde Django pondrá los archivos al hacer collectstatic (producción)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Carpeta donde Django busca los archivos estáticos durante desarrollo
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core', 'static'),  # <- aquí
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
