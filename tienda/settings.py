68e4e25a46d8da2672218484e9eef2ca13b5f0dc
Martin Leonardo Martinez Hernandez <martin.martinezh@colvatel.com>
2025-09-05 11:30:38 -0500

    ahora si

--- tienda/settings.py
+++ tienda/settings.py
@@ -19,7 +19,7 @@ AUTHENTICATION_BACKENDS = [
 SECRET_KEY = 'django-insecure-fl8*n7z&+j2#e#n^&#mmda@3vq%xv4)avy5+@d#(0(*mh@xlz*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['kaure.pythonanywhere.com', 'www.kaure.pythonanywhere.com']
 
